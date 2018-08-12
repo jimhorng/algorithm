cfe68d3bd19ea698fd352e1843dada0e53042b87
Jim Horng <jimhorng@gmail.com>
2018-08-12 08:49:30 +0800

    Create solution_binary_search.py

---
+++ 774. Minimize Max Distance to Gas Station/solution_binary_search.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minmaxGasDist(self, stations, K):
+        def possible(D):
+            return sum(int((stations[i+1] - stations[i]) / D)
+                       for i in xrange(len(stations) - 1)) <= K
+
+        lo, hi = 0, 10**8
+        while hi - lo > 1e-6:
+            mi = (lo + hi) / 2.0
+            if possible(mi):
+                hi = mi
+            else:
+                lo = mi
+        return lo
