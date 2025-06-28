c0363c4ee763d3d74868177298d7f03a1362ac83
Jim Horng <jimhorng@gmail.com>
2025-06-29 00:31:30 +0800

    Update solution.py

--- 432. All O`one Data Structure/solution.py
+++ 432. All O`one Data Structure/solution.py
@@ -1,3 +1,6 @@
+from typing import Set
+
+
 class Node:
     def __init__(self, count, keys: Set):
         self.next = None
@@ -5,86 +8,84 @@ class Node:
         self.count = count
         self.keys: Set = keys
 
+
 class AllOne:
 
     def __init__(self):
-        self.first = Node(0, set()) # dummy
-        self.last = Node(0, set()) # dummy
+        self.first = Node(0, set())  # dummy
+        self.last = Node(0, set())  # dummy
         self.first.next = self.last
         self.last.prev = self.first
         self.key_cnt = {}
         self.cnt_node = {}
 
     def inc(self, key: str) -> None:
-        cnt_org = self.key_cnt.get(key, 0)
-        cnt_new = cnt_org + 1
-        self.key_cnt[key] = cnt_new
-        node_org = self.cnt_node.get(cnt_org)
-        node_prev = node_org
-        # if new key, org node not exist
-        if not node_org:
-            node_prev = self.first
-        else:
-            if self._remove_key_from_node(key, node_org, cnt_org):
-                node_prev = node_org.prev
-            # print_nodes_line(self.first)
-        node_new = self.cnt_node.get(cnt_new)
-        # if new node's not exists, add node
-        if not node_new:
-            node_new = self._add_node(node_prev, cnt_new)
-        node_new.keys.add(key)
-        # print(key)
-        # print_nodes_line(self.first)
-        
+        old_count = self.key_cnt.get(key, 0)
+        new_count = old_count + 1
+        self.key_cnt[key] = new_count
+
+        old_node = self.cnt_node.get(old_count)
+        insert_after = old_node if old_node else self.first
+
+        self._move_key_to_count(key, new_count, insert_after)
+
+        if old_node:
+            self.remove_key_from_node(key, old_node, old_count)
+
     def dec(self, key: str) -> None:
-        cnt_org = self.key_cnt.get(key, 0)
-        cnt_new = cnt_org - 1
-        self.key_cnt[key] = cnt_new
-        if self.key_cnt[key] == 0:
-            del self.key_cnt[key]
-        node_org = self.cnt_node.get(cnt_org)
-        node_next = node_org
-        if self._remove_key_from_node(key, node_org, cnt_org):
-            node_next = node_org.next
-        # if new count is 0, skip
-        if cnt_new != 0:
-            node_new = self.cnt_node.get(cnt_new)
-            # if new node's not exists, add node
-            if not node_new:
-                node_new = self._add_node(node_next.prev, cnt_new)
-            node_new.keys.add(key)
-        # print(key)
-        # print_nodes_line(self.first)
+        old_count = self.key_cnt[key]
+        new_count = old_count - 1
+        old_node = self.cnt_node[old_count]
+
+        self._update_key_count(key, new_count)
+
+        if new_count > 0:
+            self._move_key_to_count(key, new_count, old_node.prev)
+
+        self.remove_key_from_node(key, old_node, old_count)
 
     def getMaxKey(self) -> str:
-        if self.first.next == self.last:
-            return ""
-        return next(iter(self.last.prev.keys))
+        return self._get_key_from_node(self.last.prev)
 
     def getMinKey(self) -> str:
-        if self.first.next == self.last:
+        return self._get_key_from_node(self.first.next)
+
+    def _move_key_to_count(self, key: str, count: int, insert_after: Node) -> None:
+        """Add key to node with given count, creating node if necessary."""
+        node = self.cnt_node.get(count)
+        if not node:
+            node = Node(count, set())
+            # Insert new_node after insert_after in the linked list
+            next_node = insert_after.next
+            insert_after.next = node
+            next_node.prev = node
+            node.prev = insert_after
+            node.next = next_node
+            self.cnt_node[count] = node
+        node.keys.add(key)
+
+    def _update_key_count(self, key: str, new_count: int) -> None:
+        """Update or remove key count in tracking dictionary."""
+        if new_count == 0:
+            del self.key_cnt[key]
+        else:
+            self.key_cnt[key] = new_count
+
+    def _get_key_from_node(self, node: Node) -> str:
+        """Get any key from node, or empty string if node is dummy."""
+        if node == self.first or node == self.last:
             return ""
-        return next(iter(self.first.next.keys))
-    
-    def _remove_key_from_node(self, key, node_org, cnt_org):
-        node_org.keys.remove(key)
-        # if org node's key is empty, remove node
-        if len(node_org.keys) == 0:
-            node_org.prev.next = node_org.next
-            node_org.next.prev = node_org.prev
-            del self.cnt_node[cnt_org]
+        return next(iter(node.keys))
+
+    def remove_key_from_node(self, key, node, cnt):
+        node.keys.remove(key)
+        if not node.keys:
+            # Remove node from linked list and tracking dictionary
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            del self.cnt_node[cnt]
             return True
         return False
-    
-    def _add_node(self, node_prev, cnt_new):
-        node_new = Node(cnt_new, set())
-        node_next = node_prev.next
-        node_prev.next = node_new
-        node_next.prev = node_new
-        node_new.prev = node_prev
-        node_new.next = node_next
-        self.cnt_node[cnt_new] = node_new
-        return node_new
 
 
 # Your AllOne object will be instantiated and called as such:
