0ba6de168b244f7a76baeea12350b7235635d309
Jim Horng <jimhorng@gmail.com>
2025-11-24 09:21:04 +0800

    Update recursive.py

--- 1106. Parsing A Boolean Expression/recursive.py
+++ 1106. Parsing A Boolean Expression/recursive.py
@@ -1,35 +1,38 @@
 class Solution:
+    BOOLS = ("t", "f")
+    OPERATORS = ("|", "&", "!")
+
     def parseBoolExpr(self, expression: str) -> bool:
-        BOOLS = ("t", "f")
-        OPERATORS = ("|", "&", "!")
-
-        def _calc(operator, bools):
-            if operator == '|':
-                return any(bools)
-            elif operator == '&':
-                return all(bools)
-            elif operator == '!':
-                return not bools[0]
-
-        def _f(start):
-            """ ret: result bool, last idx """
-            cur = start
-            operator = expression[cur]
-            cur += 2 # skip operator, (
-            bools = []
-            while expression[cur] != ')':
-                if expression[cur] in BOOLS:
-                    bools.append(True if expression[cur] == 't' else False)
-                elif expression[cur] == ',':
-                    pass
-                elif expression[cur] in OPERATORS:
-                    bool_val, cur = _f(cur)
-                    bools.append(bool_val)
-                cur += 1
-            bool_val = _calc(operator, bools)
-            return bool_val, cur
-
         # no operator
-        if expression[0] in BOOLS:
-            return True if expression[0] == 't' else False
-        return _f(0)[0]
+        if expression[0] in self.BOOLS:
+            return self._convert(expression[0])
+        return self._f(0, expression)[0]
+
+    def _convert(self, char):
+        return True if char == 't' else False
+
+    def _calc(self, operator, bools):
+        if operator == '|':
+            return any(bools)
+        elif operator == '&':
+            return all(bools)
+        elif operator == '!':
+            return not bools[0]
+
+    def _f(self, start, expr):
+        """ ret: result bool, last idx """
+        cur = start
+        operator = expr[cur]
+        cur += 2 # skip operator, (
+        bools = []
+        while expr[cur] != ')':
+            if expr[cur] in self.BOOLS:
+                bools.append(self._convert(expr[cur]))
+            elif expr[cur] == ',':
+                pass
+            elif expr[cur] in self.OPERATORS:
+                bool_val, cur = self._f(cur, expr)
+                bools.append(bool_val)
+            cur += 1
+        bool_val = self._calc(operator, bools)
+        return bool_val, cur
